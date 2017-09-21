42bbb3bcc38795471768928716e2e5a54b05c8c8
ejhigson <e.higson@mrao.cam.ac.uk>
2017-09-21 17:21:52 +0100

    with modified formatting of settings and test

--- pns_settings.py
+++ pns_settings.py
@@ -1,13 +1,39 @@
 #!/usr/bin/python
-"""Defines a class to hold variables and functions describing the problem's likelihood and priors."""
+"""
+Defines base class which holds settings for performing perfect nested sampling.
+This inludes likelihood and prior objects as well as the parameters controlling
+how the calculation is performed - for example the number of live points and
+whether or not dynamic nested sampling is to be used.
+"""
 
 import copy
-import pns.maths_functions as mf
 import pns.priors as priors
 import pns.likelihoods as likelihoods
 
 
 class PerfectNestedSamplingSettings(object):
+
+    """
+    Perfect Nested Sampling Settings
+
+    Keyword Arguments
+    -----------------
+    n_dim: int
+        Dimension of the likelihood and prior.
+
+    prior: class
+        A prior object containing functions logx_given_r(r, n_dim) and
+        r_given_logx(logx, n_dim) for some spherically symmetric prior
+        pi(theta) = pi(r) centred on r=0. See likelihoods.py for more
+        details.
+    likelihood: class
+        A likelihood object. See likelihoods.py for more details.
+
+    nlive: int
+        The number of live points.
+        Increasing nlive increases the number of samples taken (~ O(nlive) )
+        and therefore increases accuracy and runtime.
+    """
 
     def __init__(self, **kwargs):
 
@@ -22,9 +48,10 @@ class PerfectNestedSamplingSettings(object):
             'nlive': 100,
             'data_version': 'v01',
             'dims_to_sample': 1,
-            'zv_termination_fraction': 0.0001,  # do not write in standard form as it messes with file names
+            'zv_termination_fraction': 0.0001,
+            # dynamic nested sampling settings
+            # only used if dynamic_goal is not None.
             'dynamic_goal': None,
-            # dynamic nested sampling settings - only used if dynamic_goal is not None
             'nlive_1': 5,
             'nlive_2': 1,
             'n_calls_frac': 0,
@@ -36,60 +63,98 @@ class PerfectNestedSamplingSettings(object):
         }
 
         for (setting_name, default_value) in default_settings.items():
-            setattr(self, setting_name, kwargs.get(setting_name, default_value))
+            setattr(self,
+                    setting_name,
+                    kwargs.get(setting_name, default_value))
 
-    # functions of priors
+    # functions of the spherically symmetric prior
 
     def logx_given_r(self, r):
+        """
+        param r: radial coordinate(s)
+        type r: float, numpy array
+        returns logx: the prior's corresponding logx coordinates
+        rtype: float, numpy array (same type as input)
+        """
         return self.prior.logx_given_r(r, self.n_dim)
 
     def r_given_logx(self, logx):
+        """
+        param logx: logx coordinate(s)
+        type logx: float, numpy array
+        returns r: the prior's corresponding radial coordinates
+        rtype: float, numpy array (same type as input)
+        """
         return self.prior.r_given_logx(logx, self.n_dim)
 
-    # functions of likelihoods
+    # functions of the spherically symmetric likelihood
 
     def logl_given_r(self, r):
+        """
+        param r: radial coordinate(s)
+        type r: float, numpy array
+        returns logl: the loglikelihoods corresponding to the input r values
+        rtype: float, numpy array (same type as input)
+        """
         return self.likelihood.logl_given_r(r, self.n_dim)
 
     def r_given_logl(self, logl):
+        """
+        param logl: loglikelihood value(s)
+        type logl: float, numpy array
+        returns r: the radial coordiantes corresponding to the input
+        loglikelihood values
+        rtype: float, numpy array (same type as input)
+        """
         return self.likelihood.r_given_logl(logl, self.n_dim)
 
-    # generic functions
+    # functions of both the likelihood and the prior
 
     def logl_given_logx(self, logx):
+        """
+        param logx: logx coordinate(s)
+        type logx: float, numpy array
+        returns logl: the loglikelihoods corresponding to the input logx values
+        rtype: float, numpy array (same type as input)
+        """
         return self.logl_given_r(self.r_given_logx(logx))
 
     def logx_given_logl(self, logl):
+        """
+        param logl: loglikelihood value(s)
+        type logl: float, numpy array
+        returns logx: the prior's corresponding logx coordinates
+        rtype: float, numpy array (same type as input)
+        """
         return self.logx_given_r(self.r_given_logl(logl))
 
-    def sample_contours(self, logx):
-        """Samples {theta} given isolikelihood contours {logx} as 1D numpy array."""
-        r = self.r_given_logx(logx)
-        return mf.sample_nsphere_shells(r, self.n_dim, self.dims_to_sample)
-        # return np.hstack((samples, np.reshape(logx, (logx.shape[0], 1))))
-
     def logz_analytic(self):
+        """
+        If available gives an analytically calculated value for the log
+        evidence for the likelihood and prior (useful for checking results).
+
+        This functionality is stored in the likelihood object. If it has not
+        been set up then an error message is printed and nothing is returned.
+        """
         try:
             return self.likelihood.logz_analytic(self.prior, self.n_dim)
         except (AttributeError, AssertionError):
-            print('No logz_analytic set up for ' + type(self.likelihood).__name__ + " likelihoods and " + type(self.prior).__name__ + " priors.")
+            print('No logz_analytic set up for ' +
+                  type(self.likelihood).__name__ +
+                  " likelihoods and " + type(self.prior).__name__ + " priors.")
 
     def get_settings_dict(self):
         """
-        Returns a dictionary which contains all settings, including both class and instance variables.
-        This is needed as the __dict__ magic method does not give class variables.
+        Returns a dictionary containing settings information. The names and
+        parameters of the likelihoods and priors are stored instead of the
+        objects themselves so they can be saved with pickle.
         """
         settings_dict = copy.deepcopy(self.__dict__)
-        # replace the likelihood and prior objects with information about them in the form of strings and dicts so the output can be saved with pickle
+        # Replace the likelihood and prior objects with information about them
+        # in the form of strings and dicts so the output can be saved with
+        # pickle.
         settings_dict['likelihood'] = type(self.likelihood).__name__
         settings_dict['likelihood_args'] = self.likelihood.__dict__
         settings_dict['prior'] = type(self.prior).__name__
         settings_dict['prior_args'] = self.prior.__dict__
         return settings_dict
-
-#    def set_var(self, dictionary):
-#        """Sets all variables to values from an input dictionary."""
-#        for key, value in dictionary.items():
-#            setattr(self, key, value)
-#
-#        return dict
--- test.py
+++ test.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python
-"""Contains the functions which perform nested sampling given input from settings. These are all called from within the wrapper function nested_sampling(settings)."""
+"""
+Contains the functions which perform nested sampling given input from settings.
+These are all called from within the wrapper function
+nested_sampling(settings).
+"""
 
 import numpy as np
 import pns_settings
@@ -7,22 +11,58 @@ import pns.estimators as e
 import pns.analysis_utils as au
 import pns.parallelised_wrappers as pw
 settings = pns_settings.PerfectNestedSamplingSettings()
-np.core.arrayprint._line_width = 400
-np.set_printoptions(precision=5, suppress=True)
+np.set_printoptions(precision=5, suppress=True, linewidth=400)
 
-estimator_list = [e.logzEstimator(), e.theta1Estimator(), e.theta1squaredEstimator(), e.theta1confEstimator(0.5), e.theta1confEstimator(0.84), e.theta1confEstimator(0.975), e.rconfEstimator(0.84)]
+# def timing_decorator(some_function):
+# 
+#     """
+#     Outputs the time a function takes
+#     to execute.
+#     """
+# 
+#     def wrapper():
+#         t1 = time.time()
+#         result = some_function()
+#         t2 = time.time()
+#         return "Time it took to run the function: " + str((t2 - t1)) + "\n"
+#     return wrapper
+# 
+# 
+# def timeFunc(func):
+# 
+#     def time(*args, **kw):
+#         t_start = time.time()
+#         result = method(*args, **kw)
+#         t_end = time.time()
+#         print('%r (%r, %r) %2.2f sec' % \
+#               (method.__name__, args, kw, t_end - t_start))
+#         return result
+#     return timed
+
+
+estimator_list = [e.logzEstimator(),
+                  e.theta1Estimator(),
+                  e.theta1squaredEstimator(),
+                  e.theta1confEstimator(0.5),
+                  e.theta1confEstimator(0.84),
+                  e.theta1confEstimator(0.975),
+                  e.rconfEstimator(0.84)]
 
 print("True est values")
 print(e.check_estimator_values(estimator_list, settings))
 
 print("Standard NS Run")
-s_run_list = pw.generate_runs(settings, 10)
-stats, all_values = pw.func_on_runs(au.run_estimators, s_run_list, estimator_list)
+s_run_list = pw.generate_runs(settings, 10, parallelise=False)
+stats, all_values = pw.func_on_runs(au.run_estimators,
+                                    s_run_list,
+                                    estimator_list)
 print(stats)
 
 
 print("Dynamic NS Run")
 settings.dynamic_goal = 1
 d_run_list = pw.generate_runs(settings, 10)
-stats, all_values = pw.func_on_runs(au.run_estimators, d_run_list, estimator_list)
+stats, all_values = pw.func_on_runs(au.run_estimators,
+                                    d_run_list,
+                                    estimator_list)
 print(stats)
