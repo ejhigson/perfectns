0c0a0074954c6ebbde311cbbcec65530e4f435f4
ejhigson <e.higson@mrao.cam.ac.uk>
2017-10-06 10:14:09 +0100

    bootstrap std estimates working

--- pns/analysis_utils.py
+++ pns/analysis_utils.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 """Module for functions used to analyse the nested sampling run's outputs"""
 
+import random
 import numpy as np
-import random  # for shuffling lists
 import pns.maths_functions as mf
 
 
@@ -10,7 +10,7 @@ import pns.maths_functions as mf
 # ----------------
 
 
-def get_logx(logl, nlive_array, simulate=False):
+def get_logx(nlive_array, simulate=False):
     """
     Returns a logx vector showing the expected or simulated logx positions of
     points.
@@ -51,7 +51,7 @@ def get_logx_old(logl, nlive_array, simulate=False):
 def get_logw(logl, nlive_array, simulate=False):
     logx_inc_start = np.zeros(logl.shape[0] + 1)
     # find X value for each point
-    logx_inc_start[1:] = get_logx(logl, nlive_array, simulate=simulate)
+    logx_inc_start[1:] = get_logx(nlive_array, simulate=simulate)
     logw = np.zeros(logl.shape[0])
     for i, _ in enumerate(logw[:-1]):
         logw[i] = mf.log_subtract(logx_inc_start[i], logx_inc_start[i + 2])
@@ -78,7 +78,17 @@ def get_lp_nlive(ns_run):
     lp = combine_lp_list(ns_run[1])
     if 'nlive_array' in ns_run[0]:
         nlive_array = ns_run[0]['nlive_array']
-    else:
+    elif 'thread_logl_min_max' not in ns_run[0]:  # standard run
+        assert ns_run[0]['settings']['dynamic_goal'] is None, \
+            "dynamic ns run does not contain thread_logl_min_max!"
+        assert len(ns_run[1]) == ns_run[0]['settings']['nlive'], \
+            "nlive != number of threads"
+        nlive_array = np.zeros(lp.shape[0]) + ns_run[0]['settings']['nlive']
+        for i in range(1, ns_run[0]['settings']['nlive']):
+            nlive_array[-i] = i
+    else:  # dynamic run
+        assert ns_run[0]['settings']['dynamic_goal'] is not None, \
+            "standard ns run contains thread_logl_min_max!"
         # if nlive_array is not already stored for the run, find it iteratively
         # using the minimum and maximum logls of the slices
         nlive_array = np.zeros(lp.shape[0])
@@ -103,14 +113,15 @@ def get_lp_nlive(ns_run):
                     ind = np.where((lp[:, 0] > logl_mm[0]) &
                                    (lp[:, 0] <= logl_mm[1]))[0]
                     nlive_array[ind] += incriment
-        if nlive_array.min() < 1:
-            loglmax = np.zeros(len(ns_run[0]['thread_logl_min_max']))
-            for i, lmm in enumerate(ns_run[0]['thread_logl_min_max']):
-                loglmax[i] = lmm[1]
-            print(lp[-1, :], loglmax.max(), lp[-1, 0] == loglmax.max())
-            print(lp[:, 0])
-            assert nlive_array.min() > 0, \
-                ("nlive_array contains zeros: " + str(nlive_array))
+    # If nlive_array contains zeros then print info and throw error
+    if nlive_array.min() < 1:
+        loglmax = np.zeros(len(ns_run[0]['thread_logl_min_max']))
+        for i, lmm in enumerate(ns_run[0]['thread_logl_min_max']):
+            loglmax[i] = lmm[1]
+        print(lp[-1, :], loglmax.max(), lp[-1, 0] == loglmax.max())
+        print(lp[:, 0])
+        assert nlive_array.min() > 0, \
+            ("nlive_array contains zeros: " + str(nlive_array))
     return lp, nlive_array
 
 
@@ -173,14 +184,11 @@ def run_std_bootstrap(ns_run, estimator_list, **kwargs):
     simulate = kwargs.get('simulate', False)
     n_simulate = kwargs["n_simulate"]  # No default, must specify
     return_values = kwargs.get('return_values', False)
-    credible_interval = kwargs.get('credible_interval', False)
+    credible_int = kwargs.get('credible_int', False)
     assert return_values is True or return_values is False, \
         "return_values = " + str(return_values) + " must be True or False"
-    bootstrap_values = np.zeros((len(estimator_list), n_simulate))
-    if isinstance(ns_run[0], np.ndarray):  # run is just a list of threads
-        threads = ns_run
-    else:
-        threads = ns_run[1]
+    bs_values = np.zeros((len(estimator_list), n_simulate))
+    threads = ns_run[1]
     for i in range(0, n_simulate):
         threads_temp = []
         logl_min_max_temp = []
@@ -190,59 +198,59 @@ def run_std_bootstrap(ns_run, estimator_list, **kwargs):
         # else:  # standard ns run with live points included
         #     ns_run_temp = [{"nlive": ns_run[0]["nlive"]}, []]
         # run is just list of threads
-        if isinstance(ns_run[0], np.ndarray):  # run is just a list of threads
+        if ns_run[0]['settings']['dynamic_goal'] is not None:  # dynamic run
+            assert 'thread_logl_min_max' in ns_run[0], \
+                "dynamic ns run does not contain thread_logl_min_max!"
+            # first resample initial threads going all the way through the run
+            for j in range(0, ns_run[0]['settings']["nlive_1"]):
+                ind = random.randint(0, ns_run[0]['settings']["nlive_1"] - 1)
+                threads_temp.append(threads[ind])
+                logl_min_max_temp.append(ns_run[0]["thread_logl_min_max"][ind])
+            # now resample the remaining threads
+            for j in range(ns_run[0]['settings']["nlive_1"], len(threads)):
+                ind = random.randint(ns_run[0]['settings']["nlive_1"],
+                                     len(threads) - 1)
+                threads_temp.append(threads[ind])
+                logl_min_max_temp.append(ns_run[0]["thread_logl_min_max"][ind])
+            ns_run_temp = [{"thread_logl_min_max": logl_min_max_temp,
+                            "settings": ns_run[0]['settings']},
+                           threads_temp]
+        else:  # standard run
+            assert 'thread_logl_min_max' not in ns_run[0], \
+                "standard ns run contains thread_logl_min_max!"
             for j in range(len(threads)):
                 ind = random.randint(0, len(threads) - 1)
                 threads_temp.append(threads[ind])
-            ns_run_temp = threads_temp
-        else:
-            if "nlive_1" in ns_run[0]:  # dynamic run
-                # first resample initial threads going all the way through the run
-                for j in range(0, ns_run[0]["nlive_1"]):
-                    ind = random.randint(0, ns_run[0]["nlive_1"] - 1)
-                    threads_temp.append(threads[ind])
-                # now resample the remaining threads
-                for j in range(ns_run[0]["nlive_1"], len(threads)):
-                    ind = random.randint(ns_run[0]["nlive_1"],
-                                         len(threads) - 1)
-                    threads_temp.append(threads[ind])
-            else:  # standard run
-                for j in range(len(threads)):
-                    ind = random.randint(0, len(threads) - 1)
-                    threads_temp.append(threads[ind])
-                    if "thread_logl_min_max" in ns_run[0]:
-                        logl_min_max_temp.append(ns_run[0]["thread_logl_min_max"][ind])
-            # make resampled threads into a run
-            if "thread_logl_min_max" in ns_run[0]:
-                ns_run_temp = [{"thread_logl_min_max": logl_min_max_temp}, threads_temp]
-            else:  # standard ns run with live points included
-                nlive_temp = np.zeros(get_n_calls(threads_temp)) + len(threads)
-                for k in range(1, len(threads)):
-                    nlive_temp[-k] = k
-                ns_run_temp = [{"nlive": nlive_temp}, threads_temp]
-        lp_temp, nlive_temp = get_lp_nlive(threads_temp)
+            ns_run_temp = [{"settings": ns_run[0]['settings']},
+                           threads_temp]
+        lp_temp, nlive_temp = get_lp_nlive(ns_run_temp)
         logw_temp = get_logw(lp_temp[:, 0], nlive_temp, simulate=simulate)
-        bootstrap_values[:, i] = get_estimators(lp_temp, logw_temp, estimator_list)
-        # bootstrap_values[:, i] = (n_simulate * expected_estimators) - ((n_simulate - 1) * temp_estimators)
+        bs_values[:, i] = get_estimators(lp_temp, logw_temp, estimator_list)
         del ns_run_temp
-    if credible_interval is False:
-        stats = mf.stats_rows(bootstrap_values)
-        output = stats[:, 1]
+    if credible_int is False:
+        stds = np.zeros(bs_values.shape[0])
+        for i, _ in enumerate(stds):
+            stds[i] = np.std(bs_values[i, :], ddof=1)
+        return stds
     else:
         # estimate specificed confidence interval
         # formulae for alpha CI on estimator T = 2 T(x) - G^{-1}(T(x*))
         # where G is the CDF of the bootstrap resamples
-        assert min(credible_interval, 1. - credible_interval) * n_simulate > 1, "n_simulate=" + str(n_simulate) + " is not big enough to calculate the bootstrap " + str(credible_interval) + " CI"
+        assert min(credible_int, 1. - credible_int) * n_simulate > 1, \
+            "n_simulate=" + str(n_simulate) + " is not big enough to" \
+            "calculate the bootstrap " + str(credible_int) + " CI"
         lp, nlive = get_lp_nlive(ns_run)
         # the expected values must NOT be simulated
         logw = get_logw(lp[:, 0], nlive, simulate=False)
         expected_estimators = get_estimators(lp, logw, estimator_list)
         output = expected_estimators * 2
-        cdf = (np.asarray(range(bootstrap_values.shape[1])) + 0.5) / bootstrap_values.shape[1]
+        cdf = ((np.asarray(range(bs_values.shape[1])) + 0.5) /
+               bs_values.shape[1])
         for i, _ in enumerate(output):
-            output[i] -= np.interp(1. - credible_interval, cdf, np.sort(bootstrap_values[i, :]))
-        # print(bootstrap_values, credible_interval, cdf, output)
+            output[i] -= np.interp(1. - credible_int, cdf,
+                                   np.sort(bs_values[i, :]))
+        # print(bs_values, credible_int, cdf, output)
     if return_values is False:
         return output
     elif return_values is True:
-        return output, bootstrap_values
+        return output, bs_values
--- test.py
+++ test.py
@@ -20,7 +20,9 @@ pd.set_option('display.width', 200)
 
 # settings
 # --------
-n_repeats = 100
+n_repeats = 2000
+n_simulate = 50
+settings.dynamic_goal = 1
 settings.n_dim = 3
 estimator_list = [e.logzEstimator(),
                   e.theta1Estimator(),
@@ -35,21 +37,30 @@ print(e.check_estimator_values(estimator_list, settings))
 
 print("Standard NS Run")
 s_run_list = pw.get_run_data(settings, n_repeats)
+# get std from repeats
 rep_values = pw.func_on_runs(au.run_estimators, s_run_list, estimator_list)
 rep_df = mf.get_df_row_summary(rep_values, e_names)
 results = pd.DataFrame([rep_df.loc['std'], rep_df.loc['std_unc']],
                        index=['repeats std', 'repeats std_unc'])
+# get std from bootstrap estimate
+bs_values = pw.func_on_runs(au.run_std_bootstrap, s_run_list, estimator_list,
+                            n_simulate=n_simulate)
+bs_df = mf.get_df_row_summary(bs_values, e_names)
+results.loc['bs std'] = bs_df.loc['mean']
+results.loc['bs std_unc'] = bs_df.loc['mean_unc']
+# get std from simulation estimate
 sim_values = pw.func_on_runs(au.run_std_simulate, s_run_list, estimator_list,
-                             n_simulate=50)
+                             n_simulate=n_simulate)
 sim_df = mf.get_df_row_summary(sim_values, e_names)
 results.loc['sim std'] = sim_df.loc['mean']
 results.loc['sim std_unc'] = sim_df.loc['mean_unc']
+results = mf.df_unc_rows_to_cols(results, ["repeats std", "bs std", "sim std"])
 print(results)
 # # print("Dynamic NS Run")
 # import pns.parallelised_wrappers as pw
 # import pns.analysis_utils as au
 # import pns.maths_functions as mf
-# settings.tuned_dynamic_p = True
+# settings.dynamic_goal = 1
 # d_run_list = pw.get_run_data(settings, 10)
 # values = pw.func_on_runs(au.run_estimators, d_run_list, estimator_list)
 # d_df = mf.get_df_row_summary(values, e_names)
