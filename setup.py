00fe5b439c91556004e39902854ee9e90452711f
ejhigson <e.higson@mrao.cam.ac.uk>
2017-11-07 17:35:50 +0000

    adding setup and Makefile

---
+++ setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+
+setup(name='PerfectNestedSampling',
+      version='1.0',
+      author='Edward Higson',
+      author_email='ejhigson@gmail.com',
+      py_modules=['PerfectNestedSampling'])
